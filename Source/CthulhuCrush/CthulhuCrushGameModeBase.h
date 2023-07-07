c0001e6725f35146e0de6eef41663406f8f3c64b
NavJav256 <ec19075@qmul.ac.uk>
2023-07-07 20:44:34 +0100

    Project Init

---
+++ Source/CthulhuCrush/CthulhuCrushGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CthulhuCrushGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CTHULHUCRUSH_API ACthulhuCrushGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
